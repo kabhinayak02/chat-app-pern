468d8c29f143d7f0b91482ff75976304fcff4961
Abhinay Kumar <kabhinayak02@gmail.com>
2024-12-04 17:48:45 +0530

    edit config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://chat-app-pern-backend.onrender.com",
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
