f0f7cfaa415c62480c9536069d41cf2a64564b97
Abhinay Kumar <kabhinayak02@gmail.com>
2024-12-05 12:50:10 +0530

    update cookie config

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -9,7 +9,7 @@ const generateToken = (userId: string, res:Response) => {
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none", //  strict will block cookie to sent for cross-origin requests.
         secure: process.env.NODE_ENV !== "development"
     })
 
