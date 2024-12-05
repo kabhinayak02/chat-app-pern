6843fd2a8c544836dd5cdcc9dcfabaa4c620a70a
Abhinay Kumar <kabhinayak02@gmail.com>
2024-12-05 13:07:15 +0530

    test cookie

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -10,7 +10,7 @@ const generateToken = (userId: string, res:Response) => {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: false,
         sameSite: "none", //  strict will block cookie to sent for cross-origin requests.
-        secure: process.env.NODE_ENV !== "development"
+        secure: false
     })
 
     return token;
