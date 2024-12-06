5087b1bc6aea989c794813689b2f56988113ac1c
Abhinay Kumar <kabhinayak02@gmail.com>
2024-12-06 21:22:03 +0530

    update cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 // const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://chat-app-pern-frontend.onrender.com"],
+    origin: ["http://localhost:5173", "https://chat-app-frontend-tqgq.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
